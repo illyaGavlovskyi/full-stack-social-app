03757ce276a841e608710c22a36dc60a69531058
illyaGavlovskyi <151693791+illyaGavlovskyi@users.noreply.github.com>
2025-07-06 12:57:59 -0700

    12:57 7/6/2025

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,10 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
-import ModeToggle from "@/components/ui/ModeTogle";
 
 export default function Home() {
   return (
     <div>
-      <SignedOut>
-        <SignInButton mode="modal">
-         <Button>sign in</Button>
-        </SignInButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-      <ModeToggle />
+      <h1>Home page content</h1>
     </div>
   );
 }
