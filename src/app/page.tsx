e4d611d3c5954b8d4113bee40083911be5b3cfb6
illyaGavlovskyi <151693791+illyaGavlovskyi@users.noreply.github.com>
2025-07-08 11:38:45 -0700

    11

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 
-export default function Home() {
+export default async function Home() {
   return (
     <div>
       <h1>Home page content</h1>
