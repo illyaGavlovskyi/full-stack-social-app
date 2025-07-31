0ddb2257a6dc1747f0c7841da1f2e0661fd92f73
illyaGavlovskyi <151693791+illyaGavlovskyi@users.noreply.github.com>
2025-07-31 02:00:45 -0700

    2:00

--- src/actions/user.action.ts
+++ src/actions/user.action.ts
@@ -1,54 +1,55 @@
-"use server"
+"use server";
 
 import prisma from "@/lib/prisma";
 import { auth, currentUser } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
 
 export async function syncUser() {
-    try{
-        const {userId} = await auth()
-        const user = await currentUser();
+  try {
+    const { userId } = await auth();
+    const user = await currentUser();
 
-        if(!userId || !user) return;
+    if (!userId || !user) return;
 
-        const existingUser = await prisma.user.findUnique({
-            where: {
-                clerkId: userId
-            }
-        })
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        clerkId: userId,
+      },
+    });
 
-        if(existingUser) return existingUser
+    if (existingUser) return existingUser;
 
-        const dbUser = await prisma.user.create({
-            data: {
-                clerkId : userId,
-                name : `${user.firstName || ""} ${user.lastName || ""}`,
-                username : user.username ?? user.emailAddresses[0]?.emailAddress.split("@")[0],
-                email : user.emailAddresses[0].emailAddress,
-                image : user.imageUrl,
-            }
-        })
-    return
+    const dbUser = await prisma.user.create({
+      data: {
+        clerkId: userId,
+        name: `${user.firstName || ""} ${user.lastName || ""}`,
+        username: user.username ?? user.emailAddresses[0].emailAddress.split("@")[0],
+        email: user.emailAddresses[0].emailAddress,
+        image: user.imageUrl,
+      },
+    });
 
-    } catch (error) {
-        console.log("Error syncing user:", error);
-    }
+    return dbUser;
+  } catch (error) {
+    console.log("Error in syncUser", error);
+  }
 }
 
 export async function getUserByClerkId(clerkId: string) {
-    return prisma.user.findUnique({
-        where: {
-            clerkId,
+  return prisma.user.findUnique({
+    where: {
+      clerkId,
+    },
+    include: {
+      _count: {
+        select: {
+          followers: true,
+          following: true,
+          posts: true,
         },
-        include: {
-            _count: {
-                select: {
-                    followers: true,
-                    following: true,
-                    posts: true,
-                }
-            }
-        }
-    })
+      },
+    },
+  });
 }
 
 export async function getDbUserId() {
@@ -102,5 +103,60 @@ export async function getRandomUsers() {
   } catch (error) {
     console.log("Error fetching random users", error);
     return [];
+  }
+}
+
+export async function toggleFollow(targetUserId: string) {
+  try {
+    const userId = await getDbUserId();
+
+    if (!userId) return;
+
+    if (userId === targetUserId) throw new Error("You cannot follow yourself");
+
+    const existingFollow = await prisma.follows.findUnique({
+      where: {
+        followerId_followingId: {
+          followerId: userId,
+          followingId: targetUserId,
+        },
+      },
+    });
+
+    if (existingFollow) {
+      // unfollow
+      await prisma.follows.delete({
+        where: {
+          followerId_followingId: {
+            followerId: userId,
+            followingId: targetUserId,
+          },
+        },
+      });
+    } else {
+      // follow
+      await prisma.$transaction([
+        prisma.follows.create({
+          data: {
+            followerId: userId,
+            followingId: targetUserId,
+          },
+        }),
+
+        prisma.notification.create({
+          data: {
+            type: "FOLLOW",
+            userId: targetUserId, // user being followed
+            creatorId: userId, // user following
+          },
+        }),
+      ]);
+    }
+
+    revalidatePath("/");
+    return { success: true };
+  } catch (error) {
+    console.log("Error in toggleFollow", error);
+    return { success: false, error: "Error toggling follow" };
   }
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import CreatePost from "@/components/CreatePost";
+import WhoToFollow from "@/components/WhoToFollow";
 import { currentUser } from "@clerk/nextjs/server";
 
 export default async function Home() {
